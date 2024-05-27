9ca029ea6b8c73b73f83d5e8b6025332dae8ad10
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-05-27 17:20:31 +0900

    Headerにスタイルを当てる

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="bg-primary">
-      <p>サンプルアプリケーション</p>
+    <header className="bg-primary text-white flex justify-between items-center h-20 px-20 font-bold text-xl">
+      <p className="text-white">サンプルアプリケーション</p>
       <Link to="/">Top</Link>
     </header>
   )
