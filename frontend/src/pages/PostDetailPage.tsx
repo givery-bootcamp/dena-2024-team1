ad256dd56cf204f2d40b78ecdf5c9175d2b1c8d4
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-05-27 18:23:35 +0900

    投稿詳細画面にスタイルを当てる

--- frontend/src/pages/PostDetailPage.tsx
+++ frontend/src/pages/PostDetailPage.tsx
@@ -3,24 +3,29 @@ import { Container } from "../shared/components/Container"
 export function PostDetailPage() {
   return (
     <Container>
-      <h1>{mockPost.title}</h1>
-      <div>
-        <p>作成日時: {mockPost.createdAt}</p>
-        <p>更新日時: {mockPost.updatedAt}</p>
-      </div>
-      <hr />
-      <div>
-        <p>{mockPost.body}</p>
-        <p>{mockPost.user.username}</p>
+      <div className="flex flex-col gap-7 mt-10">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-bold text-xl">{mockPost.title}</h1>
+          <div className="flex flex-col gap-0.5 text-sm text-gray-200">
+            <p>作成日時: {mockPost.createdAt}</p>
+            <p>更新日時: {mockPost.updatedAt}</p>
+          </div>
+        </div>
+        <hr className="border-border" />
+        <div className="flex flex-col gap-3 text-lg">
+          <p className="whitespace-pre-line leading-8">{mockPost.body}</p>
+          <p className="text-right">{mockPost.user.username}</p>
+        </div>
       </div>
     </Container>
   )
 }
 
+// APIから以下のレスポンスが返ってくる想定
 const mockPost = {
   id: "1",
   title: "バナナはおやつに含まれますか？",
-  body: "来週の遠足の件で質問です。\nおやつは一人500円までと言われているのですが、バナナはおやつに含まれますか？\nそれとも弁当に含まれますか？",
+  body: "来週の遠足の件で質問です。\n\nおやつは一人500円までと言われているのですが、バナナはおやつに含まれますか？\nそれとも弁当に含まれますか？",
   createdAt: "2024/05/27 09:00",
   updatedAt: "2024/05/27 15:30",
   user: {
