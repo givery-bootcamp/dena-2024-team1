a00e8282380eeb118214b09dfc71d0e7cb210110
ayumu-abe <ayumu.abe@dena.com>
2024-05-28 01:57:14 +0900

    update: errorメッセージの規則に則る(https://go.dev/wiki/CodeReviewComments#error-strings)

--- backend/internal/controllers/hello_world_controller.go
+++ backend/internal/controllers/hello_world_controller.go
@@ -22,13 +22,13 @@ func HelloWorld(ctx *gin.Context) {
 	} else if result != nil {
 		ctx.JSON(200, result)
 	} else {
-		handleError(ctx, 404, errors.New("Not found"))
+		handleError(ctx, 404, errors.New("not found"))
 	}
 }
 
 func validateHelloWorldParameters(lang string) error {
 	if len(lang) != 2 {
-		return errors.New(fmt.Sprintf("Invalid lang parameter: %s", lang))
+		return fmt.Errorf("invalid lang parameter: %s", lang)
 	}
 	return nil
 }
