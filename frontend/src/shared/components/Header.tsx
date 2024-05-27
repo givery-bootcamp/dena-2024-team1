b2eb9f9967da5f55053298bb24a1f0095226d171
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-05-27 17:21:50 +0900

    Headerのpaddingを調整する

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="bg-primary text-white flex justify-between items-center h-20 px-20 font-bold text-xl">
+    <header className="bg-primary text-white flex justify-between items-center h-20 px-12 font-bold text-xl">
       <p className="text-white">サンプルアプリケーション</p>
       <Link to="/">Top</Link>
     </header>
