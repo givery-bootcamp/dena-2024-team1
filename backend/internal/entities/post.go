6f14e36a8608ae4298206edd0399260221181345
noboru-nishimoto <noboru.nishimoto@dena.com>
2024-05-27 17:12:21 +0900

    エンティティ入れた

---
+++ backend/internal/entities/post.go
@@ -0,0 +1,13 @@
+package entities
+
+import "time"
+
+type Post struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Title     string    `json:"title"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"update_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
