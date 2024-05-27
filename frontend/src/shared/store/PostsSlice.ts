d74a7e43bedcdf9e92be2c95e24a23e75acc8137
tadashi-nishio <tadashi.nishio@dena.jp>
2024-05-27 15:20:24 +0900

    投稿一覧を表示する機能を追加

---
+++ frontend/src/shared/store/PostsSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { Post } from '../models';
+import { APIService } from '../services';
+
+export type PostsState = {
+  posts?: Post[];
+};
+
+export const initialState: PostsState = {};
+
+export const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(APIService.getPosts.fulfilled, (state, action) => {
+      state.posts = action.payload;
+    });
+  },
+});
+
+export default postsSlice.reducer;
--- frontend/src/shared/store/index.ts
+++ frontend/src/shared/store/index.ts
@@ -1,15 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import helloReducer, { helloSlice } from './HelloSlice';
+import postsReducer, { postsSlice } from './PostsSlice';
 
 export const store = configureStore({
   reducer: {
     hello: helloReducer,
+    posts: postsReducer,
   },
 });
 
 export const actions = {
   ...helloSlice.actions,
+  ...postsSlice.actions,
 };
 
 export type RootState = ReturnType<typeof store.getState>;
