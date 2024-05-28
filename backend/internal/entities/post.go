611eff2c3a6e218c64ed552916d15879871a86e5
noboru-nishimoto <noboru.nishimoto@dena.com>
2024-05-28 16:41:39 +0900

    ドメインモデルの変更

--- backend/internal/entities/post.go
+++ backend/internal/entities/post.go
@@ -3,9 +3,10 @@ package entities
 import "time"
 
 type Post struct {
-	UserID    int       `json:"user_id"`
+	ID        int       `json:"id"`
 	Title     string    `json:"title"`
 	Body      string    `json:"body"`
+	UserName  string    `json:"user_name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"update_at"`
 }
