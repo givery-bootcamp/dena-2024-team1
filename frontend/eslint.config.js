5141d7549ffa1718b5744f05af3cbc3422ed3290
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-05-28 13:46:58 +0900

    import順を統一する

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 import tailwind from "eslint-plugin-tailwindcss"
-
+import importPlugin from "eslint-plugin-import";
 
 export default [
   {languageOptions: { globals: globals.browser }},
@@ -13,11 +13,23 @@ export default [
   ...fixupConfigRules(pluginReactConfig),
   ...tailwind.configs["flat/recommended"],
   {
+    plugins: {
+      import: importPlugin
+    }
+  },
+  {
     rules: {
       "react/jsx-uses-react": "off",
       "react/react-in-jsx-scope": "off",
       "semi": "error",
-      "quotes": "error"
+      "quotes": "error",
+      "import/order": [
+        "error",
+        {
+          groups: ['builtin', 'external', 'internal', 'sibling', 'parent', 'index', 'object'],
+          'newlines-between': 'always',
+        }
+      ]
     },
     settings: {
       react: {
