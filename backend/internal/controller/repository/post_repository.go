9a0f0c584246ddf4458280f81b21d667d379b9e0
noboru-nishimoto <noboru.nishimoto@dena.com>
2024-06-17 14:03:48 +0900

    コントローラーを弄った

--- backend/internal/controller/repository/post_repository.go
+++ backend/internal/controller/repository/post_repository.go
@@ -28,12 +28,12 @@ func NewPostRepository(conn *gorm.DB) repositoryIF.PostRepository {
 	}
 }
 
-func (r *PostRepository) CreatePost(post entity.Post) (entity.Post, error) {
-	postResult := r.Conn.Create(&post)
+func (r *PostRepository) CreatePost(post *entity.Post) (entity.Post, error) {
+	postResult := r.Conn.Create(post)
 	if postResult.Error != nil {
 		return entity.Post{}, postResult.Error
 	}
-	return post, nil
+	return *post, nil
 }
 
 func (r *PostRepository) GetAll() ([]entity.Post, error) {
