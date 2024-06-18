ac86ed54580221cd783d02f5e760d1e2bab271cc
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-18 15:59:35 +0900

    feat: 投稿編集画面の雛形を作成する

--- frontend/src/app/AppRoute.tsx
+++ frontend/src/app/AppRoute.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import { PostListPage } from "~/pages/PostListPage";
 import { PostDetailPage } from "~/pages/PostDetailPage";
+import { PostEditPage } from "~/pages/PostEditPage";
 
 export const AppRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<PostListPage />} />
       <Route path="/posts/:postId" element={<PostDetailPage />} />
+      <Route path="/posts/:postId/edit" element={<PostEditPage />} />
     </Routes>
   );
 };
---
+++ frontend/src/pages/PostEditPage.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const PostEditPage: FC = () => {
+  return <div>PostEditPage</div>;
+};
