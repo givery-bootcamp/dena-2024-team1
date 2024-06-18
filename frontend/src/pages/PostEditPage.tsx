63052d7a91e2932918e755774900846c69b65d85
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-18 16:33:05 +0900

    feat: 投稿編集画面に必要な要素を配置する

--- frontend/src/pages/PostEditPage.tsx
+++ frontend/src/pages/PostEditPage.tsx
@@ -1,5 +1,23 @@
 import type { FC } from "react";
 
+import { Button } from "~/shared/components/Button";
+import { Container } from "~/shared/components/Container";
+
 export const PostEditPage: FC = () => {
-  return <div>PostEditPage</div>;
+  return (
+    <Container>
+      <form>
+        <label>
+          タイトル
+          <input type="text" name="title" />
+        </label>
+        <label>
+          本文
+          <input type="text" name="body" />
+        </label>
+
+        <Button type="submit">更新する</Button>
+      </form>
+    </Container>
+  );
 };
