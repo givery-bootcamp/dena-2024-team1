38a62bfa43d2ae160b6f71f0530c8a8b76d7f6cc
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-18 16:51:07 +0900

    refactor: フォーム本体を別コンポーネントに分割する

--- frontend/src/pages/PostEditPage.tsx
+++ frontend/src/pages/PostEditPage.tsx
@@ -1,23 +1,12 @@
 import type { FC } from "react";
 
-import { Button } from "~/shared/components/Button";
+import { PostEditForm } from "~/features/posts/PostEditForm";
 import { Container } from "~/shared/components/Container";
 
 export const PostEditPage: FC = () => {
   return (
     <Container className="pt-10">
-      <form className="flex flex-col gap-8">
-        <label className="flex flex-col gap-1">
-          <span className="font-bold">タイトル</span>
-          <input className="h-10 rounded border border-gray-200 p-2" type="text" name="title" /> 
-        </label>
-        <label className="flex flex-col gap-1">
-          <span className="font-bold">本文</span>
-          <textarea className="rounded border border-gray-200 p-2" name="body" />
-        </label>
-
-        <Button className="h-12" type="submit">更新する</Button>
-      </form>
+      <PostEditForm />
     </Container>
   );
 };
