96ebce94e76fe985dfcdd6d1793c179214bee9fc
ayumu-abe <ayumu.abe@dena.com>
2024-06-18 09:51:56 +0900

    update: 現状のコードを自動生成に置き換え

--- backend/internal/controller/handler/post_handler.go
+++ backend/internal/controller/handler/post_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"errors"
+	"myapp/internal/openapi"
 	"myapp/internal/usecase"
 	"strconv"
 
@@ -19,11 +20,23 @@ func NewPostHandler(pu usecase.PostUsecase) PostHandler {
 }
 
 func (h *PostHandler) GetPosts(ctx *gin.Context) {
-	result, err := h.pu.GetPosts()
+	ps, err := h.pu.GetPosts()
+	var response openapi.GetAllPostsResponse
+	for _, p := range ps {
+		response = append(response, openapi.Post{
+			Body:      &p.Body,
+			CreatedAt: &p.CreatedAt,
+			Id:        &p.ID,
+			Title:     &p.Title,
+			UpdatedAt: &p.UpdatedAt,
+			UserName:  &p.UserName,
+		})
+	}
+
 	if err != nil {
 		handleError(ctx, 500, err)
-	} else if result != nil {
-		ctx.JSON(200, result)
+	} else if response != nil {
+		ctx.JSON(200, response)
 	} else {
 		handleError(ctx, 404, errors.New("not found"))
 	}
@@ -34,11 +47,22 @@ func (h *PostHandler) GetPost(ctx *gin.Context) {
 	if err != nil {
 		handleError(ctx, 400, err)
 	}
-	result, err := h.pu.GetPost(id)
+
+	p, err := h.pu.GetPost(id)
 	if err != nil {
 		handleError(ctx, 500, err)
-	} else if result != nil {
-		ctx.JSON(200, result)
+	}
+
+	if p != nil {
+		response := openapi.GetPostResponse{
+			Body:      &p.Body,
+			CreatedAt: &p.CreatedAt,
+			Id:        &p.ID,
+			Title:     &p.Title,
+			UpdatedAt: &p.UpdatedAt,
+			UserName:  &p.UserName,
+		}
+		ctx.JSON(200, response)
 	} else {
 		handleError(ctx, 404, errors.New("not found"))
 	}
