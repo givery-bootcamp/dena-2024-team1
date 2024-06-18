937dac69da44831169de16ca2c57c399c8215921
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-18 17:13:15 +0900

    ボタンを追加 スタイルを変更

--- frontend/src/pages/CreatePostPage.tsx
+++ frontend/src/pages/CreatePostPage.tsx
@@ -1,25 +1,41 @@
+import { useState } from "react";
+
 import { Container } from "~/shared/components/Container";
+import { Button } from "~/shared/components/Button";
 
 export function CreatePostPage() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(event.target.value);
+  };
+
   return (
-    <Container className="mt-10">
-      <div className="rounded-lg bg-white p-8 shadow-md">
+    <Container className="mt-20">
+      <div>
         <form>
           <div>
-            <label>
+            <label className="text-2xl font-bold">
               タイトル
             </label>
             <br/>
-            <input type="text" value="" />
+            <input type="text" value={title} onChange={handleTitleChange} className="w-full rounded border border-gray-200 text-2xl" />
           </div>
           <br/>
           <div>
-            <label>
+            <label className="text-2xl font-bold">
               本文
             </label>
             <br/>
-            <textarea value="" />
+            <textarea value={content} onChange={handleContentChange} className="h-64 w-full rounded border border-gray-200 text-2xl" />
           </div>
+          <br/>
+          <Button className="w-full" type="submit">投稿する</Button>
         </form>
       </div>
     </Container>
