31e84696ef482cf4b454a5604f2162ed4544c977
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-18 16:44:21 +0900

    feat: 投稿編集フォームにスタイルを当てる

--- frontend/src/pages/PostEditPage.tsx
+++ frontend/src/pages/PostEditPage.tsx
@@ -5,18 +5,18 @@ import { Container } from "~/shared/components/Container";
 
 export const PostEditPage: FC = () => {
   return (
-    <Container>
-      <form>
-        <label>
-          タイトル
-          <input type="text" name="title" />
+    <Container className="pt-10">
+      <form className="flex flex-col gap-8">
+        <label className="flex flex-col gap-1">
+          <span className="font-bold">タイトル</span>
+          <input className="h-10 rounded border border-gray-200 p-2" type="text" name="title" /> 
         </label>
-        <label>
-          本文
-          <input type="text" name="body" />
+        <label className="flex flex-col gap-1">
+          <span className="font-bold">本文</span>
+          <textarea className="rounded border border-gray-200 p-2" name="body" />
         </label>
 
-        <Button type="submit">更新する</Button>
+        <Button className="h-12" type="submit">更新する</Button>
       </form>
     </Container>
   );
