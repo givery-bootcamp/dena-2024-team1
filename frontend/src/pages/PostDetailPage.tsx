ea624391d4b40f2bccd9acab09760f8ce8a7f07e
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-18 14:40:42 +0900

    postがundefinedだった場合に早期リターンするように変更

--- frontend/src/pages/PostDetailPage.tsx
+++ frontend/src/pages/PostDetailPage.tsx
@@ -10,23 +10,26 @@ export function PostDetailPage() {
   const { post } = useAppSelector((state) => state.post);
   const dispatch = useAppDispatch();
   const { postId } = useParams<{postId: string}>();
+
   useEffect(() => {
     dispatch(APIService.getPost(Number(postId)));
   }, [dispatch]);
+
+  if (!post) return <p>Loading...</p>;
   return (
     <Container>
       <div className="mt-10 flex flex-col gap-7">
         <div className="flex flex-col gap-2">
-          <h1 className="text-xl font-bold">{post?.title}</h1>
+          <h1 className="text-xl font-bold">{post.title}</h1>
           <div className="flex flex-col gap-0.5 text-sm text-gray-200">
-            <p>作成日時: { formatDateTime(post?.createdAt ?? "") }</p>
-            <p>更新日時: { formatDateTime(post?.updatedAt ?? "")}</p>
+            <p>作成日時: { formatDateTime(post.createdAt) }</p>
+            <p>更新日時: { formatDateTime(post.updatedAt)}</p>
           </div>
         </div>
         <hr className="border-border" />
         <div className="flex flex-col gap-3 text-lg">
-          <p className="whitespace-pre-line leading-8">{post?.body}</p>
-          <p className="text-right">{post?.userName}</p>
+          <p className="whitespace-pre-line leading-8">{post.body}</p>
+          <p className="text-right">{post.userName}</p>
         </div>
       </div>
     </Container>
