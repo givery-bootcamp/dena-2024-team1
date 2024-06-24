ba8e8a20f1bccb83dd8e6c650fcf57affec88da3
ayumu-abe <ayumu.abe@dena.com>
2024-06-24 17:20:51 +0900

    remove: 'DeletedAt' field from the 'Post' entity

--- backend/internal/entity/post.go
+++ backend/internal/entity/post.go
@@ -10,5 +10,4 @@ type Post struct {
 	UserName  string    `json:"user_name"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	DeletedAt time.Time `json:"deleted_at"`
 }
