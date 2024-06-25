edabffea39f924d503e28bedceb46a9030b382c6
ayumu-abe <ayumu.abe@dena.com>
2024-06-25 18:34:11 +0900

    update: これで大丈夫なはず

--- mysql/sql/seed.sql
+++ mysql/sql/seed.sql
@@ -1,13 +1,15 @@
 DELETE FROM hello_worlds;
+DELETE FROM sketches;
+DELETE FROM posts;
+DELETE FROM users;
+
+
 INSERT INTO hello_worlds (lang, message) VALUES ('en', 'Hello World'), ('ja', 'こんにちは 世界');
 
-DELETE FROM users;
 INSERT INTO users (name, password) VALUES ('taro', 'password'),('hanako', 'PASSWORD');
 
-DELETE FROM posts;
 INSERT INTO posts (user_id, title, body) VALUES (1, 'test1', '質問1\n改行'),(1, 'test2', '質問2\n改行');
 
-DELETE FROM sketches;
 INSERT INTO sketches (image_name, user_id) VALUES
 ('happy.png', 1),
 ('quickMTG.png', 1),
