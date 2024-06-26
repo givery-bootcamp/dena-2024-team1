cf501603f11f619129989ecc60d48da9af829a98
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-26 11:55:00 +0900

    画像一覧の文字を削除 不要なコメントアウトを削除

--- frontend/src/pages/ImageListPage.tsx
+++ frontend/src/pages/ImageListPage.tsx
@@ -4,8 +4,6 @@ import { useAppDispatch, useAppSelector } from "~/shared/hooks";
 import { APIService } from "~/shared/services";
 import { Container } from "~/shared/components/Container";
 import { SketchItem } from "~/features/sketches/SketchItem";
-//import { BorderLine } from "~/shared/components/BorderLine";
-//import { Sketch } from "~/generated";
 
 export function ImageListPage() {
   const { sketches } = useAppSelector((state) => state.sketches);
@@ -18,7 +16,6 @@ export function ImageListPage() {
   
   return (
     <Container className='mt-10'>
-      <h1 className='text-2xl font-bold'>画像一覧</h1>
       <div className='grid grid-cols-3'>
         {sketches?.map((sketch,index) => (
           <Fragment key={index}>
