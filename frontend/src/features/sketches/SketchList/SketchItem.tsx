02f2a70f318b2e077d7b0279a15d3d21f58c4850
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-27 14:02:25 +0900

    Modalを開けるようになった

--- frontend/src/features/sketches/SketchList/SketchItem.tsx
+++ frontend/src/features/sketches/SketchList/SketchItem.tsx
@@ -1,3 +1,6 @@
+import { useState }from "react";
+
+import { Modal } from "~/shared/components/Modal";
 import type { Sketch } from "~/generated";
 
 type SketchItemProps = {
@@ -5,7 +8,16 @@ type SketchItemProps = {
 };
 
 export const SketchItem = ({ sketch }: SketchItemProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <img className="h-[200px] w-[300px] border border-gray-200 object-cover" src={sketch.imageUrl} alt={sketch.imageUrl} />
+    <div onClick={() => setIsModalOpen(true)}>
+      <img className="h-[200px] w-[300px] border border-gray-200 object-cover" src={sketch.imageUrl} alt={sketch.imageUrl} />
+      <div onClick={(event) => event.stopPropagation()}>
+        <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+          <img src={sketch.imageUrl} alt={sketch.imageUrl} />
+          {/* <button onClick={() => console.log("clicked")}>閉じる</button> */}
+        </Modal>
+      </div>
+    </div>
   );
 };
