ce7b8b5c59cb527825eb8860f1e3a9ca9e9a4769
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-27 17:33:10 +0900

    インデント修正 returnを追加

--- frontend/src/features/sketches/SketchModal.tsx
+++ frontend/src/features/sketches/SketchModal.tsx
@@ -1,10 +1,14 @@
 import { Modal } from "~/shared/components/Modal";
 type Props = {
-    selectedSketchUrl: string;
-    setSelectedSketchUrl: (url: string | null) => void;
+  selectedSketchUrl: string;
+  setSelectedSketchUrl: (url: string | null) => void;
 };
 
-export const SketchModal = ({ selectedSketchUrl, setSelectedSketchUrl }: Props) =>
-  <Modal isOpen={true} onClose={() => setSelectedSketchUrl(null)}>
-    <img src={selectedSketchUrl} alt={selectedSketchUrl} />
-  </Modal>;
+export const SketchModal = ({ selectedSketchUrl, setSelectedSketchUrl }: Props) => {
+  return (
+    <Modal isOpen={true} onClose={() => setSelectedSketchUrl(null)}>
+      <img src={selectedSketchUrl} alt={selectedSketchUrl} />
+    </Modal>
+  );
+};
+
