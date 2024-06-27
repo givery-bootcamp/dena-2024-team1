b5841d3a8411a0d237c936310c6ba36cdf30eb78
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-27 11:33:16 +0900

    feat: スタイルを整える

--- frontend/src/features/users/UserAuthForm/UserAuthForm.tsx
+++ frontend/src/features/users/UserAuthForm/UserAuthForm.tsx
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 
 import { type Schema, schema } from "./schema";
 
+import { Button } from "~/shared/components/Button";
+
 type UserAuthFormProps = {
   onSubmit: (username: string, password: string) => void;
   title: string;
@@ -29,18 +31,22 @@ export const UserAuthForm = ({ title, submitText, linkText, linkTo, onSubmit }: 
         <h1 className="text-xl font-bold">{ title }</h1>
       </div>
       <hr className="border-border" />
-      <div className="flex flex-col gap-3 text-lg">
+      <div className="flex flex-col gap-6 text-lg">
         <label className="flex flex-col gap-2 text-lg">
-          <span>ユーザー名</span>
-          <input className="border border-border p-2" type="text" {...register("username")} />
-          {formState.errors.username && <span className="text-sm text-alert">{formState.errors.username.message}</span>}
+          <span className="font-bold">ユーザー名</span>
+          <div className="flex flex-col gap-1">
+            <input className="border border-border p-2" type="text" {...register("username")} />
+            {formState.errors.username && <span className="text-sm font-semibold text-alert">{formState.errors.username.message}</span>}
+          </div>
         </label>
         <label className="flex flex-col gap-2 text-lg">
-          <span>パスワード</span>
-          <input className="border border-border p-2" type="password" {...register("password")} />
-          {formState.errors.password && <span className="text-sm text-alert">{formState.errors.password.message}</span>}
+          <span className="font-bold">パスワード</span>
+          <div className="flex flex-col gap-1">
+            <input className="border border-border p-2" type="password" {...register("password")} />
+            {formState.errors.password && <span className="text-sm font-semibold text-alert">{formState.errors.password.message}</span>}
+          </div>
         </label>
-        <button className="mt-10 rounded-md bg-primary p-2 text-white" type="submit">{submitText}</button>
+        <Button type="submit">{submitText}</Button>
       </div>
       <div>
         <Link to={linkTo} className="underline">{linkText}</Link>
