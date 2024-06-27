70c94c6990e60ff8c34d87aa5ee43f4ac0b15cfa
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-27 15:08:09 +0900

    refactor: Recordを使わない

--- frontend/src/features/users/UserAuthForm/mapping.ts
+++ frontend/src/features/users/UserAuthForm/mapping.ts
@@ -1,22 +1,18 @@
 export type PageType = "sign-in" | "sign-up";
 
-type Title = "ログイン" | "新規登録";
-type PageLink = "/signin" | "/signup";
-type ButtonLabel = "ログインする" | "アカウントを作成する";
-
-export const typeToTitle: Record<PageType, Title> = {
+export const typeToTitle = {
   "sign-in": "ログイン",
   "sign-up": "新規登録",
-};
-export const typeToButtonLabel: Record<PageType, ButtonLabel> = {
+} as const;
+export const typeToButtonLabel = {
   "sign-in": "ログインする",
   "sign-up": "アカウントを作成する",
-};
-export const typeToLink: Record<PageType, PageLink> = {
+} as const;
+export const typeToLink = {
   "sign-in": "/signup",
   "sign-up": "/signin",
-};
-export const typeToNavigationText: Record<PageType, string> = {
+} as const;
+export const typeToNavigationText = {
   "sign-in": "アカウントをお持ちでない方はこちら",
   "sign-up": "アカウントをお持ちの方はこちら",
-};
+} as const;
