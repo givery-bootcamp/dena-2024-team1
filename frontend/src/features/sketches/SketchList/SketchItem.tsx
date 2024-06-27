d031869f85caf01f941248cc5050f0301c97aec0
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-27 16:45:13 +0900

    useAtomをuseSetAtomに変更

--- frontend/src/features/sketches/SketchList/SketchItem.tsx
+++ frontend/src/features/sketches/SketchList/SketchItem.tsx
@@ -1,4 +1,4 @@
-import { useAtom } from "jotai";
+import { useSetAtom } from "jotai";
 
 import type { Sketch } from "~/generated";
 import { SelectedSketchUrl } from "~/shared/store/Sketch";
@@ -8,7 +8,7 @@ type SketchItemProps = {
 };
 
 export const SketchItem = ({ sketch }: SketchItemProps) => {
-  const [, setSelectedSketchUrl] = useAtom(SelectedSketchUrl);
+  const setSelectedSketchUrl = useSetAtom(SelectedSketchUrl);
   return (
     <div onClick={() => setSelectedSketchUrl(sketch.image_url)}>
       <img className="h-[200px] w-[300px] border border-gray-200 object-cover" src={sketch.image_url} alt={sketch.image_url} />
