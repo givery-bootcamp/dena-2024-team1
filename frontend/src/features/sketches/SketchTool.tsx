55c07853053bc34e98d17eb1d6b2907aa20b47c6
Wataru Ono <44804976+watagit@users.noreply.github.com>
2024-06-28 11:49:47 +0900

    feat: キャンパスが大きすぎる印象だったため小さくした

--- frontend/src/features/sketches/SketchTool.tsx
+++ frontend/src/features/sketches/SketchTool.tsx
@@ -3,7 +3,7 @@ import { SketchColorPicker } from "./SketchColorPicker";
 export const SketchTool = () => {
   return (
     <div className="flex w-56 flex-col gap-8">
-      <h5 className="text-xl font-bold">お絵描きツール</h5>
+      <h5 className="text-lg font-bold">お絵描きツール</h5>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col items-center gap-2">
           <p className="w-full text-sm font-semibold text-gray-200">線の色</p>
