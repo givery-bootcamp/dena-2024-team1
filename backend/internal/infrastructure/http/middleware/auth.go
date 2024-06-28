7591f67811fb7c4aca57e7d232003e5f440c2e09
tomoya.kosugi <tomoya.kosugi@dena.com>
2024-06-28 14:44:02 +0900

    csrf対策のために、認証が必要なリクエストの前にプリフライトリクエストを送る

--- backend/internal/infrastructure/http/middleware/auth.go
+++ backend/internal/infrastructure/http/middleware/auth.go
@@ -9,6 +9,16 @@ import (
 
 func Auth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		// ヘッダーからX-Custom-Header-For-Preflightを取得（フロントエンドがPreflightリクエストを強制するために設定しているヘッダー）
+		xCustomHeaderForPreflight := ctx.GetHeader("X-Custom-Header-For-Preflight")
+
+		// X-Custom-Header-For-Preflightが設定されていない場合はエラーを返す
+		if xCustomHeaderForPreflight != "true" {
+			ctx.JSON(401, gin.H{"error": "Not Set X-Custom-Header-For-Preflight"})
+			ctx.Abort()
+			return
+		}
+
 		session := sessions.Default(ctx)
 
 		if session.Get(config.SessionKey) == nil {
--- backend/internal/infrastructure/http/middleware/cors.go
+++ backend/internal/infrastructure/http/middleware/cors.go
@@ -11,5 +11,8 @@ func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{myConfig.CorsAllowOrigin}
 	config.AllowCredentials = true
+	defaultAllowHeaders := cors.DefaultConfig().AllowHeaders
+	// X-Custom-Header-For-Preflightを追加
+	config.AllowHeaders = append(defaultAllowHeaders, "X-Custom-Header-For-Preflight")
 	return cors.New(config)
 }
