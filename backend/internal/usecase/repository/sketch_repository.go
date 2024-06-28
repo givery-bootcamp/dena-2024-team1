3c142f0a0db4cd95709306069910f74af4f28631
noboru-nishimoto <noboru.nishimoto@dena.com>
2024-06-28 17:25:47 +0900

    Revert "repository"
    This reverts commit ecc985e458555434214aaf83fbe7c0e40d8e4f5c.

--- backend/internal/usecase/repository/sketch_repository.go
+++ backend/internal/usecase/repository/sketch_repository.go
@@ -9,5 +9,4 @@ import (
 type SketchRepository interface {
 	CreateSketch(ctx context.Context, file *multipart.File, userID int) (*entity.Sketch, error)
 	GetAll(ctx context.Context) ([]entity.Sketch, error)
-	DeleteSketch(ctx context.Context, id int) error
 }
