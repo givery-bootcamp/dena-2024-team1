1129d6a97f162ef5a985e82a0ff8ead1d350af29
tadashi-nishio <tadashi.nishio@dena.jp>
2024-06-28 13:27:21 +0900

    ユーザー名の表示場所を変更

--- frontend/src/features/sketches/SketchModal.tsx
+++ frontend/src/features/sketches/SketchModal.tsx
@@ -1,5 +1,6 @@
 import { Modal } from "~/shared/components/Modal";
 import { Sketch } from "~/generated";
+
 type Props = {
   selectedSketch: Sketch;
   setSelectedSketch: (sketch: Sketch | null) => void;
@@ -8,9 +9,12 @@ type Props = {
 export const SketchModal = ({ selectedSketch, setSelectedSketch }: Props) => {
   return (
     <Modal isOpen={true} onClose={() => setSelectedSketch(null)}>
-      <img src={selectedSketch.image_url} alt={selectedSketch.image_url} />
-      <h1>{selectedSketch.user_name}</h1>
+      <div className="flex h-full flex-col">
+        <img src={selectedSketch.image_url} alt={selectedSketch.image_url} />
+        <div className="absolute bottom-2 right-2 text-xl font-semibold text-gray-200">
+          {selectedSketch.user_name}
+        </div>
+      </div>
     </Modal>
   );
 };
-
