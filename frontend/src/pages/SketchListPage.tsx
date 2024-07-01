e35c8df67f6dc8247eb897eb76018cb54de8d5ff
tadashi-nishio <tadashi.nishio@dena.jp>
2024-07-01 11:08:01 +0900

    ちょっと上に移動

--- frontend/src/pages/SketchListPage.tsx
+++ frontend/src/pages/SketchListPage.tsx
@@ -20,7 +20,7 @@ export function SketchListPage() {
   if (!sketches) return <p>Loading...</p>;
   if (sketches.length === 0) {
     return (
-      <div className="flex h-screen-without-header flex-col items-center justify-center space-y-4">
+      <div className="mt-[-20px] flex h-screen-without-header flex-col items-center justify-center space-y-4">
         <img src="./alert-circle.svg" alt="alert" className="mx-auto mt-20 size-20" />
         <div className="text-4xl text-gray-200">まだ投稿がありません</div>
       </div>
